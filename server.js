ec245862f4ef85668bd42bd4948af75934daa7c3
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:22:27 -0500

    Added the express dependency and created an express server

---
+++ server.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+const express = require('express');
+
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// Tells node that we are creating an "express" server
+const app = express();
+
+
