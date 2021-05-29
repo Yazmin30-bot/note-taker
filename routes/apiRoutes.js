91b5fe82ac6e33f6fe1fe9ca829dc8b628c59194
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:05:42 -0500

    Imported the fs and uuid dependencies to read/write json file and create an unique id, and create the routing

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+
+// imported 'fs' npm package for read/write files
+const fs = require("fs");
+// imported 'uuid' npm package for unique id
+const { v4: uuidv4 } = require('uuid');
+
+
+
+// ROUTING
+
+module.exports = (app) => {
+
+  
+};
