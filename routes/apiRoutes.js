5466e260034d82bf17449b1aae9fc954b6771fc3
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:09:03 -0500

    Create a get request to show the data in the table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,12 @@ const { v4: uuidv4 } = require('uuid');
 // ROUTING
 
 module.exports = (app) => {
+  //Create a get request to show the data in the table
+  app.get('/api/notes', (req, res) => {
+    //read
+    // We are linking our routes to a series of "data" sources. using fs package
+    let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(noteData);
+  })
 
-  
 };
