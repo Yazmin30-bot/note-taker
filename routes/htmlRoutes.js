c3f44d8c1abd4a3f1446d9a4c6dd944cced18191
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:19:18 -0500

    Show default to home when the matching route is found

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,8 +8,13 @@ const path = require('path');
 module.exports = (app) => {
   // => HTML GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
+  // In each of the below cases the user is shown an HTML page of content using get request
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // If no matching route is found default to home
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 };
