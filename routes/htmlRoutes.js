29a3baa18913915222711bdb05c4698ce5e38eb6
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:17:37 -0500

    shown an HTML page of content using get request

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,5 +6,10 @@ const path = require('path');
 // ROUTING
 
 module.exports = (app) => {
-  
+  // => HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
 };
