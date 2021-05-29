e82c0b6c0f24c85f21009dce1a11dfdf37e27aca
Yazmin Tinoco <ytip30@gmail.com>
2021-05-29 02:25:25 -0500

    Sets an initial port which uses later in our listener

--- server.js
+++ server.js
@@ -7,4 +7,7 @@ const express = require('express');
 // Tells node that we are creating an "express" server
 const app = express();
 
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3000;
+
 
